730098399f51a385840082c115bf13ac971415e8
Alexander McKerrow <mckerrow4@gmail.com>
2021-07-05 17:35:16 +1200

    working on blog template

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,15 +1,41 @@
 import React from "react"
 import './CSS/Main.css'
-
+import testphoto from '../images/test.jpeg'
 function Blog () {
     return (
         <div className="sectionContainer">
-            <div className="blogPostContainer">
+            <div className="bPContainer">
                 <h1>title</h1>
                 <h2>subtitle</h2>
                 <p></p>
-                <div className="blogPostImagContainer">
-                    <img src="" alt="" />
+                <div className="bPImageContainer">
+                    <img src={testphoto} alt="test" />
+                </div>
+            </div>
+            <div className="bPContainer">
+                <div className="bP">
+                    <h1>title</h1>
+                    <h2>subtitle</h2>
+                    <p></p>
+                    <div className="bPImageContainer">
+                        <img src={testphoto} alt="test" />
+                    </div>
+                </div>
+            </div>
+            <div className="bPContainer">
+                <h1>title</h1>
+                <h2>subtitle</h2>
+                <p></p>
+                <div className="bPImageContainer">
+                    <img src={testphoto} alt="test" />
+                </div>
+            </div>
+            <div className="bPContainer">
+                <h1>title</h1>
+                <h2>subtitle</h2>
+                <p></p>
+                <div className="bPImageContainer">
+                    <img src={testphoto} alt="test" />
                 </div>
             </div>
         </div >
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ function Home () {
     return (
         <div className="App">
             <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quis eligendi ut quidem in. Perferendis veritatis illum molestias, eos, est ab culpa amet reprehenderit molestiae sint fugiat aliquid aliquam pariatur?</h1>
+
         </div>
     )
 }
