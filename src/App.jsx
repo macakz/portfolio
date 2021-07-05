777eb1a292eca40dddb6c3c97c308cce5743cdb8
Alexander McKerrow <mckerrow4@gmail.com>
2021-07-05 15:33:57 +1200

    work component added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import './App.css';
 
+//home component 
 import Home from './components/Home';
+
+//header & footer components
 import Header from './components/Header';
+import Footer from './components/Footer';
+
+//information components
 import Blog from './components/Blog';
 import Contact from './components/Contact'
-import Footer from './components/Footer';
+import Work from './components/Work';
 
 function App () {
   return (
@@ -16,6 +22,7 @@ function App () {
         <Route exact path="/" component={Home} />
         <Route path="/Blog" component={Blog} />
         <Route path="/Contact" component={Contact} />
+        <Route path="/Work" component={Work} />
       </Switch>
       <Footer />
     </div>
---
+++ src/components/Work.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Work () {
+
+    return (
+        <div>
+            <p>test</p>
+        </div>
+    )
+}
+
+export default Work
