02b0923f0e497db5b298a0b222f769c63539a421
Alexander McKerrow <mckerrow4@gmail.com>
2021-07-05 15:47:14 +1200

    lorem added to work

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,19 +8,12 @@ function Contact () {
             <div className="contactDetails">
             <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quis eligendi ut quidem in. Perferendis veritatis illum molestias, eos, est ab culpa amet reprehenderit molestiae sint fugiat aliquid aliquam pariatur?</h1> 
             <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quis eligendi ut quidem in. Perferendis veritatis illum molestias, eos, est ab culpa amet reprehenderit molestiae sint fugiat aliquid aliquam pariatur?</h1>                
-
             <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quis eligendi ut quidem in. Perferendis veritatis illum molestias, eos, est ab culpa amet reprehenderit molestiae sint fugiat aliquid aliquam pariatur?</h1>                
-
             <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quis eligendi ut quidem in. Perferendis veritatis illum molestias, eos, est ab culpa amet reprehenderit molestiae sint fugiat aliquid aliquam pariatur?</h1>                
-
             <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quis eligendi ut quidem in. Perferendis veritatis illum molestias, eos, est ab culpa amet reprehenderit molestiae sint fugiat aliquid aliquam pariatur?</h1>                
-
             <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quis eligendi ut quidem in. Perferendis veritatis illum molestias, eos, est ab culpa amet reprehenderit molestiae sint fugiat aliquid aliquam pariatur?</h1>                
-
             <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quis eligendi ut quidem in. Perferendis veritatis illum molestias, eos, est ab culpa amet reprehenderit molestiae sint fugiat aliquid aliquam pariatur?</h1>                
-
             <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quis eligendi ut quidem in. Perferendis veritatis illum molestias, eos, est ab culpa amet reprehenderit molestiae sint fugiat aliquid aliquam pariatur?</h1>                
-
             </div>
         </div>
     )
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -5,7 +5,14 @@ function Work () {
 
     return (
         <div>
-            <p>test</p>
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quis eligendi ut quidem in. Perferendis veritatis illum molestias, eos, est ab culpa amet reprehenderit molestiae sint fugiat aliquid aliquam pariatur?</h1> 
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quis eligendi ut quidem in. Perferendis veritatis illum molestias, eos, est ab culpa amet reprehenderit molestiae sint fugiat aliquid aliquam pariatur?</h1>                
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quis eligendi ut quidem in. Perferendis veritatis illum molestias, eos, est ab culpa amet reprehenderit molestiae sint fugiat aliquid aliquam pariatur?</h1>                
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quis eligendi ut quidem in. Perferendis veritatis illum molestias, eos, est ab culpa amet reprehenderit molestiae sint fugiat aliquid aliquam pariatur?</h1>                
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quis eligendi ut quidem in. Perferendis veritatis illum molestias, eos, est ab culpa amet reprehenderit molestiae sint fugiat aliquid aliquam pariatur?</h1>                
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quis eligendi ut quidem in. Perferendis veritatis illum molestias, eos, est ab culpa amet reprehenderit molestiae sint fugiat aliquid aliquam pariatur?</h1>                
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quis eligendi ut quidem in. Perferendis veritatis illum molestias, eos, est ab culpa amet reprehenderit molestiae sint fugiat aliquid aliquam pariatur?</h1>                
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quis eligendi ut quidem in. Perferendis veritatis illum molestias, eos, est ab culpa amet reprehenderit molestiae sint fugiat aliquid aliquam pariatur?</h1>                
         </div>
     )
 }
