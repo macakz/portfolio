d86f1310e5ecb8fc78cc8d7174166e9ff6df3840
Alexander McKerrow <mckerrow4@gmail.com>
2021-07-06 14:46:51 +1200

    emailjs working on contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import emailjs from 'emailjs-com';
 
 
 function Contact () {
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
+    const onSubmit = data => emailjs.send('service_8advsz8', 'contactForm', data, 'user_209MN1gR3y2lDleTZJdhx')
+        .then((response) => {
+            console.log('SUCCESS!', response.status, response.text);
+        }, (err) => {
+            console.log('FAILED...', err);
+        });
     return (
         <div className="contactContainer">
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="inputContainer">
                     <p>Name:</p>
-                    <input {...register("name", { required: true })} />
+                    <input {...register("fromName", { required: true })} />
                 </div>
                 <div className="inputContainer">
                     <p>Email:</p>
 
-                    <input  {...register("email", { required: true })} />
+                    <input  {...register("fromEmail", { required: true })} />
+                </div>
+                <div className="inputContainer">
+                    <p>Phone Number:</p>
+                    <input  {...register("phoneNumber", { required: true })} />
                 </div>
                 <div className="inputContainer">
                     <p>Subject:</p>
@@ -24,7 +34,7 @@ function Contact () {
                 </div>
                 <div className="inputContainer">
                     <p>Message:</p>
-                    <input {...register("exampleRequired", { required: true })} />
+                    <input {...register("message", { required: true })} />
                 </div>
                 <input type="submit" />
             </form>
