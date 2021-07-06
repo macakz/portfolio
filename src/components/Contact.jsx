cc365f0c3026985493959d918b81c3c4d293b1cf
Alexander McKerrow <mckerrow4@gmail.com>
2021-07-06 15:54:33 +1200

    errors and validation added for contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ import { userID, serviceID } from './config'
 
 function Contact () {
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data => emailjs.send( serviceID , 'contactForm', data, userID)
+    const onSubmit = data => emailjs.send(serviceID, 'contactForm', data, userID)
         .then((response) => {
             console.log('SUCCESS!', response.status, response.text);
         }, (err) => {
@@ -16,24 +16,39 @@ function Contact () {
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="inputContainer">
                     <p>Name:</p>
-                    <input {...register("fromName", { required: true })} />
+                    <input {...register("fromName", { required: true, pattern: /^[A-Za-z]+$/i })} />
                 </div>
+                {errors.fromName?.type === "required" && <span>This field is required</span>}
+                {errors.fromName?.type === "pattern" && <span>Please enter a valid name</span>}
+
                 <div className="inputContainer">
                     <p>Email:</p>
-                    <input  {...register("fromEmail", { required: true })} />
+                    <input  {...register("fromEmail", { required: true, pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ })} />
                 </div>
+                {errors.fromEmail?.type === "required" && <span>This field is required</span>}
+                {errors.fromEmail?.type === "pattern" && <span>Please enter a valid email</span>}
+
                 <div className="inputContainer">
                     <p>Phone Number:</p>
-                    <input  {...register("phoneNumber", { required: true })} />
+                    <input  {...register("phoneNumber", { required: true, pattern: /([1-9][0-9]*)|0/  })} />
                 </div>
+                {errors.phoneNumber?.type === "required" && <span>This field is required</span>}
+                {errors.phoneNumber?.type === "pattern" && <span>Please enter a valid phone number</span>}
+
                 <div className="inputContainer">
                     <p>Subject:</p>
-                    <input  {...register("subject", { required: true })} />
+                    <input  {...register("subject", { required: true, pattern: /^[a-z0-9]+$/i })} />
                 </div>
+                {errors.phoneNumber?.type === "required" && <span>This field is required</span>}
+                {errors.phoneNumber?.type === "pattern" && <span>Please only enter numbers or letters</span>}
+
                 <div className="inputContainer">
                     <p>Message:</p>
-                    <input {...register("message", { required: true })} />
+                    <input {...register("message", { required: true, pattern: /^[a-z0-9]+$/i })} />
                 </div>
+                {errors.message?.type === "required" && <span>This field is required</span>}
+                {errors.message?.type === "pattern" && <span>Please only enter numbers or letters</span>}
+
                 <input type="submit" />
             </form>
         </div>
