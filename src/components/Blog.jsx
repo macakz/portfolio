67e225ec4c8fe8ab2f6333f52bc9e9816757b629
Alexander McKerrow <mckerrow4@gmail.com>
2021-07-07 16:30:09 +1200

    can loop through posts

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,22 +1,29 @@
 import React from "react"
 import './CSS/Main.css'
 import testphoto from '../images/test.jpeg'
+import posts from '../post.json'
 function Blog () {
+
     return (
         <>
-            <div className="blogPost">
-                <div className="flexbox">
-                    <div className="content">
-                        <h1>title</h1>
-                        <h3>date</h3>
-                        <p>content</p>
-                        <button>Read more</button>
+            {posts.map(post => {
+                return (
+                    <div className="blogPost">
+                        <div className="flexbox">
+                            <div className="content">
+                                <h1>{post.title}</h1>
+                                <h3>{post.date}</h3>
+                                <p>{post.content}</p>
+                                <button>Read more</button>
+                            </div>
+                            <div className="imageContainer">
+                                <img src={testphoto} alt="" />
+                            </div>
+                        </div>
                     </div>
-                    <div className="imageContainer">
-                        <img src={testphoto} alt="" />
-                    </div>
-                </div>
-            </div>
+                )
+            })}
+
         </>
 
     )
