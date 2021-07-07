05b267045961bc22d002d95df3e8f742cc376af6
Alexander McKerrow <mckerrow4@gmail.com>
2021-07-07 15:45:10 +1200

    made some style global and other minor styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,7 +14,9 @@ function Contact () {
     return (
 
         <div className="contactContainer">
-            <h1>Send me a message</h1>
+            <div className="contactTitle">
+                <h1>Send me a message</h1>
+            </div>
             <div className="formContainer">
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="inputContainer">
@@ -22,7 +24,7 @@ function Contact () {
                         <input {...register("fromName", { required: true, pattern: /^[A-Za-z]+$/i })} />
                     </div>
                     <div className="error">
-                        {errors.fromName?.type === "required" && <p>This field is required</p>}
+                        {errors.fromName?.type === "required" && <p>Name field is required</p>}
                         {errors.fromName?.type === "pattern" && <p>Please enter a valid name</p>}
                     </div>
                     <div className="inputContainer">
@@ -30,7 +32,7 @@ function Contact () {
                         <input  {...register("fromEmail", { required: true, pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ })} />
                     </div>
                     <div className="error">
-                        {errors.fromEmail?.type === "required" && <p>This field is required</p>}
+                        {errors.fromEmail?.type === "required" && <p>Email field is required</p>}
                         {errors.fromEmail?.type === "pattern" && <p>Please enter a valid email</p>}
                     </div>
                     <div className="inputContainer">
@@ -38,7 +40,7 @@ function Contact () {
                         <input  {...register("phoneNumber", { required: true, pattern: /([1-9][0-9]*)|0/ })} />
                     </div>
                     <div className="error">
-                        {errors.phoneNumber?.type === "required" && <p>This field is required</p>}
+                        {errors.phoneNumber?.type === "required" && <p>Phone Number field is required</p>}
                         {errors.phoneNumber?.type === "pattern" && <p>Please enter a valid phone number</p>}
                     </div>
                     <div className="inputContainer">
@@ -46,7 +48,7 @@ function Contact () {
                         <input  {...register("subject", { required: true, pattern: /^[a-z0-9]+$/i })} />
                     </div>
                     <div className="error">
-                        {errors.phoneNumber?.type === "required" && <p>This field is required</p>}
+                        {errors.phoneNumber?.type === "required" && <p>Subject field is required</p>}
                         {errors.phoneNumber?.type === "pattern" && <p>Please only enter numbers or letters</p>}
                     </div>
                     <div className="inputContainer">
