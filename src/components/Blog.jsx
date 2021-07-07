d13b652b7c653b566dddf0db52c011efd1c099f5
Alexander McKerrow <mckerrow4@gmail.com>
2021-07-07 18:18:27 +1200

    working on blog to blogpost

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -2,19 +2,23 @@ import React from "react"
 import './CSS/Main.css'
 import testphoto from '../data/images/test.jpeg'
 import posts from '../data/blogPosts.json'
+import { Link, } from "react-router-dom";
+
 function Blog () {
 
     return (
         <>
             {posts.map(post => {
                 return (
-                    <div className="blogPost">
+                    <div className="blog">
                         <div className="flexbox">
                             <div className="content">
                                 <h1>{post.title}</h1>
                                 <h3>{post.date}</h3>
                                 <p>{post.blurb}</p>
-                                <button onClick={() => {}}>Read more</button>
+                                <div className="readMore">
+                                    <Link to="BlogPost">Read More</Link>
+                                </div>
                             </div>
                             <div className="imageContainer">
                                 <img src={testphoto} alt="" />
--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import './CSS/Main.css'
 
-import { Link, } from "react-router-dom";
 
 
 function BlogPost () {
