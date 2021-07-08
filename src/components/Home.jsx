c408436e02a795a3ad04e5da6b4e4f33703fc3b2
Alexander McKerrow <mckerrow4@gmail.com>
2021-07-08 16:30:41 +1200

    homepage grabbing latest post

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,29 @@
 import React from "react"
 import './CSS/Main.css'
 import testphoto from '../data/images/test.jpeg'
+import posts from '../data/blogPosts.json'
 
 function Home () {
+    const latestPost = posts.slice(-1)
     return (
         <>
+
             <div className="banner">
                 <div className="flexbox">
                     <div className="imageContainer">
                         <img src={testphoto} alt="" />
                     </div>
                     <div className="content">
-                        <h1>title</h1>
-                        <h3>date</h3>
-                        <p>content</p>
-                        <button>Readmore</button>
+                        <h1>{latestPost[0].title}</h1>
+                        <h3>{latestPost[0].date}</h3>
+                        <p>{latestPost[0].blurb}</p>
+                        <a href={latestPost[0].code}>View the code for {latestPost[0].title} here</a>
+                        {latestPost[0].deploy
+                            ?
+                            <a className="flexbox" href={latestPost[0].deploy}>View the deployed project!</a>
+                            : null
+                        }
                     </div>
-
                 </div>
             </div>
         </>
