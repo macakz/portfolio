291de959486d07a3de7ce4eb74385ca9ec61f815
Alexander McKerrow <mckerrow4@gmail.com>
2021-07-10 19:14:21 +1200

    can access instram api

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import { useState } from "react";
 import profile from '../data/images/profile.jpg'
 import resume from '../data/Resume.pdf'
-
+import instagramFeedConfig from "./instagramFeedConfig"
+import axios from 'axios'
 function About () {
+    const sendQuery = async (query) => {
+        let data = await axios.get(`https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,username,timestamp&access_token=${instagramFeedConfig.accessToken}`) 
+        console.log(data)
+    }
+    
+    sendQuery()
 
     return (
         <>
+        
             <div className="about" >
                 <img src={profile} alt="profilepicture" />
                 <div className="flexbox">
