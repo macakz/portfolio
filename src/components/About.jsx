a7c5700b9d8ee0038250ce5bc4b4d4fc6bff7229
Alexander McKerrow <mckerrow4@gmail.com>
2021-07-10 19:24:53 +1200

    working on displaying feed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,17 +3,24 @@ import profile from '../data/images/profile.jpg'
 import resume from '../data/Resume.pdf'
 import instagramFeedConfig from "./instagramFeedConfig"
 import axios from 'axios'
+
 function About () {
+    const [instaFeed, setInstaFeed] = useState('')
+
     const sendQuery = async (query) => {
         let data = await axios.get(`https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,username,timestamp&access_token=${instagramFeedConfig.accessToken}`) 
-        console.log(data)
+        setInstaFeed(data.data.data)
     }
     
-    sendQuery()
+
 
     return (
         <>
-        
+        {instaFeed.map(insta =>{
+            return(
+                <div>{insta}</div>
+            )
+        })}
             <div className="about" >
                 <img src={profile} alt="profilepicture" />
                 <div className="flexbox">
