bcdfa72fb5bcbfbea774403760d09a6188c22c22
Alexander McKerrow <mckerrow4@gmail.com>
2021-07-11 19:21:21 +1200

    Revert "deloyment changes"
    This reverts commit 37cfe73e35fcdf1410a17a275bdb0d1e33b03467.

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App () {
         <div className="content-wrap">
           <Header />
           <Switch>
-            <Route exact path="/" component={Home} />
+            <Route exact path="/portfolio" component={Home} />
             <Route path="/blog" component={Blog} />
             <Route path="/contact" component={Contact} />
             <Route path="/about" component={About} />
